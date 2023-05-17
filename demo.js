58811906ad7e86b854139dac8fbdf04174b1e4ee
Tony Ivanov <telamohn@pm.me>
2023-05-17 12:51:16 +0200

    Added bling and started keygen state

--- demo.js
+++ demo.js
@@ -1,20 +1,44 @@
 import { decodeASL, roll } from './index.js'
 import Geohash from 'latlon-geohash'
 let elForm
-function generate (event) {
-  event.preventDefault()
-  const fd = new FormData(event.target)
-  const age = parseInt(fd.get('age'))
-  const sex = parseInt(fd.get('sex'))
-  const lat = parseFloat(fd.get('lat'))
-  const lon = parseFloat(fd.get('lon'))
-  const bits = parseInt(fd.get('bits'))
-  const location = Geohash.encode(lat, lon, 6)
-  console.log('Generating', age, sex, location)
-  const secret = roll(age, sex, location, bits, 5000)
-  console.log('Secret rolled', secret)
+let isMining = false
+/**
+ * Updates the 'result' section after/while
+ * key-generation
+ * @type {(isMining: boolean, secret: string) => void}
+ */
+function updateResult (isMining, secret) {
+  
 }
 
+function generate (event) {
+  event.preventDefault()
+  if (isMining) { // -- STOP KEYGEN
+    isMining = false
+  } else { // -- START KEYGEN
+    const fd = new FormData(event.target)
+    const age = parseInt(fd.get('age'))
+    const sex = parseInt(fd.get('sex'))
+    const lat = parseFloat(fd.get('lat'))
+    const lon = parseFloat(fd.get('lon'))
+    const bits = parseInt(fd.get('bits'))
+    const location = Geohash.encode(lat, lon, 6)
+    console.log('Generating', age, sex, location)
+    isMining = true
+    let secret = null
+    // while (!secret && isMining) {
+      secret = roll(age, sex, location, bits, 5000)
+    // }
+    console.log('Secret rolled', secret)
+    isMining = false
+  }
+}
+
+/**
+ * Attempts to get user's pubkey via window.nostr
+ * and sets the result to pubkey-input.
+ * @type {(event: Event) => Promise<void>}
+ */
 async function nip07reveal (event) {
   event.preventDefault()
   if (typeof window.nostr?.getPublicKey !== 'function') {
@@ -24,6 +48,10 @@ async function nip07reveal (event) {
   document.getElementById('inp-pk').value = pk
 }
 
+/**
+ * Fetches location from devices
+ * @type {(event: Event) => Promise<void>}
+ */
 async function fetchLocation (event) {
   event.preventDefault()
   const res = await new Promise((resolve, reject) => {
@@ -34,12 +62,21 @@ async function fetchLocation (event) {
   document.getElementById('lon').value = res.coords.longitude
 }
 
+/**
+ * Decodes public Key pasted in input and
+ * updates the preview/visualization
+ * @type {(event: Event) => void}
+ */
 function decodePublicKey (event) {
   const { value } = event.target
   console.info('TODO: decode hexstr and zbase32', value)
   const { age, sex, location } = decodeASL(value)
 }
 
+/**
+ * Attaches listeners to elements when document
+ * has finished loading.
+ */
 function boot () {
   console.log('initializing...')
   elForm = document.getElementById('constraints')
